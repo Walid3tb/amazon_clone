5e633336a5a75a9e0c6c5b0225be7bbe45807b48
Walid3tb <160321550+Walid3tb@users.noreply.github.com>
2024-05-14 17:11:40 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
         <h1 className="md:text-7xl text-3xl text-center mb-8 font-bold">
           مرحبا بك في وصلت
         </h1>
-        <p className="md:w-1/2 w-full text-center text-sm md:text-base">
+        <h3 className="md:w-1/2 w-full text-center text-sm md:text-base">
           with WESELT we made it easier for you to pick up your kids from
           school. no more traffic, no more waiting.
-        </p>
-        <p className="md:w-1/4 w-full text-center mt-2 text-sm md:text-base">
+        </h3>
+        <h3 className="md:w-1/4 w-full text-center mt-2 text-sm md:text-base">
           مع وصلت خليناها اسهل عليك تاخذ اطفالك من المدرسة انتظار اقل زحمة اقل
-        </p>
+        </h3>
         {authUser?.role?.name === "user" && (
           <Link
             href="/students"
