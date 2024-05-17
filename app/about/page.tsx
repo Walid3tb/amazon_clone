a1158b7b81688a6b290c5e305e603b35328814c3
Walid3tb <160321550+Walid3tb@users.noreply.github.com>
2024-05-17 13:28:57 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ const About = (props: Props) => {
   return (
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <h1 className="text-4xl text-center my-10 font-bold">About Us</h1>
-      <h2 className="text-2xl font-semibold mb-4">Welcome to <span className="font-bold">WESELT!</span></h2>
+      <h2 className="text-2xl font-semibold mb-4">Welcome to <span className="font-bold">WESELT websit</span></h2>
       <h3>
         This project aims to solve an issue that all school students and their
         guardians face everyday, which is traffic and time consuming when trying
